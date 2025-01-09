ceb9e556855fbfcbfa1a2bade25ce1e36a659455
李铭昕 <715557344@qq.com>
2025-01-09 18:01:13 +0800

    增加解析 NullTime

--- support/sql.go
+++ support/sql.go
@@ -27,3 +27,20 @@ func (s *SqlSupport) NullTimeDateTimeString(value sql.NullTime) string {
 func (s *SqlSupport) NullTimeDateString(value sql.NullTime) string {
 	return s.NullTimeString(value, time.DateOnly)
 }
+
+func (s *SqlSupport) NewNullTimeByString(value string, layout string) sql.NullTime {
+	t, err := time.ParseInLocation(layout, value, time.Local)
+	if err != nil {
+		return sql.NullTime{}
+	}
+
+	return sql.NullTime{Time: t, Valid: true}
+}
+
+func (s *SqlSupport) NewNullTimeByDateString(value string) sql.NullTime {
+	if len(value) < 10 {
+		return sql.NullTime{}
+	}
+
+	return s.NewNullTimeByString(value[0:10], time.DateOnly)
+}
--- support/sql_test.go
+++ support/sql_test.go
@@ -12,3 +12,16 @@ func TestNullTimeString(t *testing.T) {
 	assert.Equal(t, "2025-01-01", NewSqlSupport().NullTimeDateString(sql.NullTime{Time: ts, Valid: true}))
 	assert.Equal(t, "", NewSqlSupport().NullTimeDateString(sql.NullTime{Valid: false}))
 }
+
+func TestNewNullTimeByDateString(t *testing.T) {
+	nt := NewSqlSupport().NewNullTimeByDateString("2025-01-01")
+	assert.Equal(t, "2025-01-01", nt.Time.Format(time.DateOnly))
+	assert.Equal(t, int64(1735660800), nt.Time.Unix())
+	assert.True(t, nt.Valid)
+
+	nt = NewSqlSupport().NewNullTimeByDateString("2025-01-01 08:00:00")
+	assert.True(t, nt.Valid)
+
+	nt = NewSqlSupport().NewNullTimeByDateString("asdf")
+	assert.False(t, nt.Valid)
+}
